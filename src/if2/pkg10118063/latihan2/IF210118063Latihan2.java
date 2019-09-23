bb0b6a5a537a9a7c0655323165a933298e46ead5
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:50:48 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan2/IF210118063Latihan2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan2;
+
+/**
+ *
+ * @author
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; 
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai pada variabel
+        nilaiInt = 29;
+        nilaiLogika = true;
+        nilaiKarakter = 'S';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai    :" + nilaiInt);
+        System.out.println("Isi variabel Phi      :" +PHI);
+        System.out.println("Isi variabel logika   :" +nilaiLogika);
+        System.out.println("Isi variabel karakter :" +nilaiKarakter);
+    }
+    
+}
